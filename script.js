3e0ac82934aee96dddecba81a01b8b082c0d5c1f
WineB17 <wineberry17@gmail.com>
2026-01-27 10:47:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ const customTextInput = document.getElementById("custom-text");
 // Load saved settings
 function loadSettings() {
   const backgroundColor = localStorage.getItem('background-color') || '#FFFFFF';
-  const fontColor = localStorage.getItem('font-color') || '#292623';
-  const fontFamily = localStorage.getItem('font-family') || 'Karla';
-  const customText = localStorage.getItem('custom-text') || 'Be kind';
+  const fontColor = localStorage.getItem('font-color') || '#EB5757';
+  const fontFamily = localStorage.getItem('font-family') || 'Grotesk';
+  const customText = localStorage.getItem('custom-text') || 'Oh, look at the time!';
   
   document.body.style.backgroundColor = backgroundColor;
   text.style.color = fontColor;
