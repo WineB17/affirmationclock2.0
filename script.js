d150ed95119bb6d1863fe97332cd3d9a07a768f7
WineB17 <wineberry17@gmail.com>
2026-02-05 18:33:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const customTextInput = document.getElementById("custom-text");
 
 // Load saved settings
 function loadSettings() {
-  const backgroundColor = localStorage.getItem('background-color') || '#FFFFFF';
+  const backgroundColor = localStorage.getItem('background-color') || 'transparent';
   const fontColor = localStorage.getItem('font-color') || '#EB5757';
   const fontFamily = localStorage.getItem('font-family') || '"Times New Roman", Times, serif';
   const customText = localStorage.getItem('custom-text') || 'Oh, look at the time!';
